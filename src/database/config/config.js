f245bc1f7581a022c1f807fe28a5ab0b5fd39541
SamuelKoroh <47538882+SamuelKoroh@users.noreply.github.com>
2020-02-26 21:11:29 +0100

    initial commit

---
+++ src/database/config/config.js
@@ -0,0 +1,17 @@
+require ('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL'
+  }
+}
---
+++ src/database/migrations/20200224211406-create-user.js
@@ -0,0 +1,51 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        required: true
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        required: true
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      userType: {
+        type: Sequelize.ENUM,
+        values: ['user','instructor'],
+        allowNull: false,
+        required: true
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        required: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        required: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  down: (queryInterface) =>  queryInterface.dropTable('Users')
+};
